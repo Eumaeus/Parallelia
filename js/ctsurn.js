305e0d7ba5f157f2c74d20584f3e4119e1976c65
Eumaeus <cwblackwell@gmail.com>
2026-07-15 22:55:44 -0400

    Refactor CtsUrn class methods for improved organization and clarity

--- js/ctsurn.js
+++ js/ctsurn.js
@@ -32,22 +32,6 @@ class CtsUrn {
   }
 
 	// -----------------------
-  // --- Basic Methods ---
-
-  toString() {
-    return `${this.urnstring}`;
-  }
-
-  equals(other) {
-  	return this.toString() == other.toString();
-  }
-
-  // Intercepts the comparison when compared to a primitive
-  [Symbol.toPrimitive](hint) {
-    return this.toString(); 
-  }
-
-	// -----------------------
   // --- URN Classification ---
 
 	// Returns true if a CtsUrn has a passage-component
@@ -96,6 +80,15 @@ class CtsUrn {
 	// -----------------------
   // --- URN Comparison ----
 
+  equals(other) {
+  	return this.toString() == other.toString();
+  }
+
+  // Intercepts the comparison when compared to a primitive
+  [Symbol.toPrimitive](hint) {
+    return this.toString(); 
+  }
+
 	// Takes another CtsUrn and returns "true" if they are equal to the version-level
 	// @param {CtsUrn} other - a CtsUrn at the version- or exemplar-level
 	// @returns {Boolean} 
@@ -129,20 +122,24 @@ class CtsUrn {
 		return false;
 	}
 
-// Takes another CtsUrn and returns "true" if (a) the bibliographic hierarchy of the `this` "includes" that of the second, and (b) the passage-components are equal.
-// @param {CtsUrn} other - a CtsUrn 
-// @returns {CtsUrn} 
-passageEquals(other) {
-	if ( !biblIncludes(this, other)) {
+	// Takes another CtsUrn and returns "true" if (a) the bibliographic hierarchy of the `this` "includes" that of the second, and (b) the passage-components are equal.
+	// @param {CtsUrn} other - a CtsUrn 
+	// @returns {CtsUrn} 
+	passageEquals(other) {
+		if ( !biblIncludes(this, other)) {
+			return false;
+		} else {
+			if (this.getPassage() == other.getPassage()) return true;
+		}
 		return false;
-	} else {
-		if (this.getPassage() == other.getPassage()) return true;
 	}
-	return false;
-}
 
 	// ---------------------
 	// --- URN Retrieval ---
+
+	toString() {
+		return `${this.urnstring}`;
+	}
 
 	// Returns the {String} of a passage-component
 	// @returns {String} 
@@ -259,99 +256,107 @@ passageEquals(other) {
 
 
 
-/**
- * A helper-function for passageIncludes(), below. Takes two string-representations of a passage-hierarchy and returns 'true' if the first includes, or "contains" the second
- * 
- * @param {String} s1
- * @param {String} s2 
- * @returns {Boolean} 
- * @throws CtsUrnError
- */
-function passageStrIncludes(s1, s2) {
-	// get components
-	let cc1 = s1.split(".");
-	let cc2 = s2.split(".");
+	// A helper-function for passageIncludes(), below. Takes two string-representations of a passage-hierarchy and returns 'true' if the first includes, or "contains" the second
+	// @param {String} s1
+	// @param {String} s2 
+	// @returns {Boolean} 
+	passageStrIncludes(s1, s2) {
+		// get components
+		let cc1 = s1.split(".");
+		let cc2 = s2.split(".");
 
-	// Easy ones…
-	if (cc1.length > cc2.length) return false;
+		// Easy one…
+		if (cc1.length > cc2.length) return false;
 
-	// Get them to the same number of fields
-	cc2 = cc2.slice(0, cc1.length);
+		// Get them to the same number of fields
+		cc2 = cc2.slice(0, cc1.length);
 
-	if (cc1.toString() == cc2.toString()) return true;
+		if (cc1.toString() == cc2.toString()) return true;
 
+		return false;
+	}
+
+
+
+	// Looks ONLY at the passage-components of two URNs. Returns true if the hierarchy expressed by the first contains that of the second. Does some careful consideration of ranges.
+	// @param {CtsUrn} other
+	// @returns {Boolean} 
+	passageIncludes(other) {
+		p1 = getPassage(this);
+		p2 = getPassage(other);
+		// easy one…
+		if (p1 == p2) return true;
+
+		// non-ranges
+
+		// A point cannot contain a range
+
+		if (!p1.isRange() && p2.isRange()) return false;
+
+		// A range can contain a point
+
+		return false;
+	}
+
+
+	// Looks at the passage-components of two URNs, `this` and `other`. Returns true if the hierarchy expressed by the first contains that of the second. Does some careful consideration of ranges. Returns false if the bibliographic hierarchy of the first does not "include" that of the second
+	// @param {CtsUrn} urn2 - a CtsUrn 
+	// @returns {Boolean} 
+	textIncludes(urn2) {
+		return false;
+	}
+
+// Given an exemplar-level CtsUrn, remove the exemplar-component of the URN, leaving everything else the same.
+// @returns {CtsUrn}
+versionFromExemplar() {
+	return null;
+}
+
+// Adds the String `exemplarId` to a version-level URN, leaving everything else unchanged.
+// @param {String} - exemplarId
+// @returns {CtsUrn}
+addExemplar(exemplarId) {
+	return null;
+}
+
+//Reduce the passage-hierarchy of the CtsUrn by one level.
+//@returns {CtsUrn}
+chopPassage() {
+	return null;
+}
+
+//Extend the passage-hierarchy of the CtsUrn by one level, adding `citeString` as the value for the new level
+//@param {String} - citeString
+//@returns CtsUrn
+extendPassage(citeString) {
+	return null;
+}
+
+//Shorthand function. Ignoring citation-components, are the URNs' bibliography-components equal.
+//@param {CtsUrn} - other
+//@returns {Boolean}
+sameText(other) {
 	return false;
 }
 
-
-
-/**
- * Looks ONLY at the passage-components of two URNs. Returns true if the hierarchy expressed by the first contains that of the second. Does some careful consideration of ranges.
- * 
- * @param {CtsUrn} urn1 - a CtsUrn 
- * @param {CtsUrn} urn2 - a CtsUrn 
- * @returns {Boolean} 
- * @throws CtsUrnError
- */
-function passageIncludes(urn1, urn2) {
-	p1 = getPassage(urn1);
-	p2 = getPassage(urn2);
-	// easy one…
-	if (p1 == p2) return true;
-	// non-ranges
-	// A point cannot contain a range
-	if (!p1.isRange() && p2.isRange()) return false;
-	// A range can contain a point
-
-
-	return false;
-
-
-}
-
-
-/**
- * Looks at the passage-components of two URNs, `this` and `other`. Returns true if the hierarchy expressed by the first contains that of the second. Does some careful consideration of ranges. Returns false if the bibliographic hierarchy of the first does not "include" that of the second
- * 
- * @param {CtsUrn} urn1 - a CtsUrn 
- * @param {CtsUrn} urn2 - a CtsUrn 
- * @returns {Boolean} 
- * @throws CtsUrnError
- */
-function textIncludes(urn1, urn2) {
-	return false;
-}
-
-
-function versionFromExemplar(urn) {
-	return "";
-}
-
-function addExemplar(urn) {
-
-}
-
-function chopPassage(urn) {
-	return "";
-}
-
-function extendPassage(citeString) {
-	return "";
-}
-
-function sameText(urn1, urn2) {
-	return false;
-}
-
-function citationLevel(urn) {
+// Returns the number of fields in the citation-component of the CtsUrn
+//@returns {Int}
+citationLevel() {
 	return 0;
 }
 
-function citationToLevel(urn, level) {
-	return "";
+//Chops the citation-hierarchy until it is `level`-levels deep.
+//Error if `level` is greater than the current citation-level.
+//@param {Int} - level
+//@returns {CtsUrn}
+citationToLevel(level) {
+	return null;
 }
 
-function equalizeCitationLevels(urn1, urn2){
-	return ["", ""];
+// Chop the citation-level of whichever URN has a deeper citation-hiearchy so that both are at the same level
+//@param {CtsUrn} - other
+//@returns [{CtsUrn}, {CtsUrn}]
+equalizeCitationLevels(other){
+	return [null, null];
 }
 
